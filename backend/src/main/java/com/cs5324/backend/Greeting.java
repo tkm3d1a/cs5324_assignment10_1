808ddb18484e4d528f27eb8cc2cdc16c96ba9504
Tim Klimpel <tim.klimpel@gmail.com>
2024-03-16 13:48:33 -0400

    Base working with support from guide

---
+++ backend/src/main/java/com/cs5324/backend/Greeting.java
@@ -0,0 +1,10 @@
+package com.cs5324.backend;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Greeting {
+    private String content;
+}
---
+++ backend/src/main/java/com/cs5324/backend/HelloMessage.java
@@ -0,0 +1,12 @@
+package com.cs5324.backend;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloMessage {
+    private String name;
+}
