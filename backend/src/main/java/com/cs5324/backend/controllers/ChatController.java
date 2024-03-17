955c728d8d0a518d2350223d5a6747d97ae054c3
MattHays2 <mhays@distorted.dev>
2024-03-17 17:46:18 -0600

    Add chat message topic and method.

--- backend/src/main/java/com/cs5324/backend/controllers/ChatController.java
+++ backend/src/main/java/com/cs5324/backend/controllers/ChatController.java
@@ -1,5 +1,6 @@
 package com.cs5324.backend.controllers;
 
+import com.cs5324.backend.chat.message.ChatMessage;
 import com.cs5324.backend.chat.message.ChatMessageService;
 import com.cs5324.backend.chat.user.ChatUser;
 import com.cs5324.backend.chat.user.ChatUserService;
@@ -24,5 +25,11 @@ public class ChatController {
     @SendTo("/topic/user")
     public ChatUser userLoggedOn(ChatUser user) {
         return userService.saveUser(user);
+    }
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/chat")
+    public ChatMessage sendChatMessage(ChatMessage message) {
+        return messageService.saveMessage(message);
     }
 }
